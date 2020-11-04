e6b56ee822c0ac74c8258c0efc242490c9c80cab
NTF121 <35421226+NTF121@users.noreply.github.com>
2020-11-04 09:33:27 +0200

    alpha_test

--- brickbraker.py
+++ brickbraker.py
@@ -4,8 +4,8 @@ pygame.init()
 resolution = 640
 win = pygame.display.set_mode((resolution, resolution))
 pygame.display.set_caption("Brickbraker")
-my=resolution-12-50
-
+my=resolution-12-20
+runn=True
 
 class bricks(object):
 	"""docstring for bricks"""
@@ -14,135 +14,149 @@ class bricks(object):
 	def brick(bx,by,bwith,bheight):
 		bricks.br = pygame.draw.rect(win, (0,0,200), (bx,by,bwith,bheight))
 		return bricks.br
-		pass
-	pass
+	brs = []
+	brs_rm = set()
+	def brickss():
+		iter_cur = 0
+		for y in range(1,5):
+			for x in range(1,19):
+				if not iter_cur in bricks.brs_rm:
+					bricks.brs.append(pygame.draw.rect(win, (187, 191, 61), (x*32,y*30,30,20)))
+				else:
+					bricks.brs.append(pygame.draw.rect(win, (187, 191, 61), (0,0,0,0)))
+				iter_cur+=1
+		for b in range(len(bricks.brs)):
+			if bricks.brs[b].colliderect(circles.cr):
+				bricks.brs_rm.add(b)
+				circles.circle_collide_brs()
+		return bricks.brs
+
+
 class circles(object):
 	"""docstring for circles"""
 	x = resolution/2
 	y = resolution/2
 	x_angle = 0.2
 	y_angle = 0.3
-	direction=0
+	direction=1
 	radius = 10
 	cr = None
 	def circle0(pos_x,pos_y):
 		circles.cr = pygame.draw.circle(win, (200,0,0), (pos_x, pos_y), circles.radius)
 		return circles.cr
-		pass
 	def circle_move():
 		circles.circle0(circles.x,circles.y)
 		if circles.direction == 0:
 			circles.x += circles.x_angle
 			circles.y += circles.y_angle
-			pass
 		elif circles.direction == 1:
 			circles.x -= circles.x_angle
 			circles.y += circles.y_angle
-			pass
 		elif circles.direction == 2:
 			circles.x -= circles.x_angle
 			circles.y -= circles.y_angle
-			pass
 		elif circles.direction == 3:
 			circles.x += circles.x_angle
 			circles.y -= circles.y_angle
-			pass
 		circles.circle_collide_br(0.1,0.1)
 		circles.circle_collide_wall()
-		pass
 	def circle_collide_wall():
 		if circles.x>resolution-circles.radius:
 			circles.x = resolution-circles.radius
-			pass
 		elif circles.y>resolution-circles.radius:
 			circles.y = resolution-circles.radius
-			pass
 		elif circles.x<circles.radius:
 			circles.x = circles.radius
-			pass
 		elif circles.y<circles.radius:
 			circles.y = circles.radius
-			pass
 		if (circles.x>resolution-circles.radius-1 and circles.direction == 0) or (circles.y<circles.radius+1 and circles.direction == 2):
 			circles.direction = 1
-			pass
 		elif (circles.y>resolution-circles.radius-1 and circles.direction == 1) or (circles.x>resolution-circles.radius-1 and circles.direction == 3): 
 			circles.direction = 2
-			pass
 		elif (circles.x<circles.radius+1 and circles.direction == 2) or (circles.y>resolution-circles.radius-1 and circles.direction == 0): 
 			circles.direction = 3
-			pass
 		elif (circles.x<circles.radius+1 and circles.direction == 1) or (circles.y<circles.radius+1 and circles.direction == 3):
 			circles.direction = 0
-			pass
-		pass
 	def circle_collide_br(x_angle_change,y_angle_change):
-		if circles.x_angle > 1 or circles.y_angle > 1 or circles.y_angle < 0.1:
-			circles.x_angle = 0.5
-			circles.y_angle = 0.5
-			pass
+		if circles.x_angle > 0.5 or circles.y_angle > 0.5 or circles.y_angle < 0.1:
+			circles.x_angle = 0.3
+			circles.y_angle = 0.2
+	
 		if bricks.br.colliderect(circles.cr) and circles.x<=controls.mx and circles.y<my-circles.radius:
 			circles.direction = 2
 			circles.x_angle -= x_angle_change
 			circles.y_angle += y_angle_change
 			circles.x = controls.mx
-			pass
+	
 		if bricks.br.colliderect(circles.cr) and circles.x>=controls.mx+bricks.bwith and circles.y<my-circles.radius:
 			circles.direction = 3
 			circles.x_angle -= x_angle_change
 			circles.y_angle += y_angle_change
 			circles.x = controls.mx+bricks.bwith
-			pass
+	
 		if bricks.br.colliderect(circles.cr) and circles.x<((controls.mx+bricks.bwith/2)+controls.mx)/2 and not circles.y<my-circles.radius:
 			circles.direction = 2
 			circles.x_angle += x_angle_change
 			circles.y_angle -= y_angle_change
-			pass
+	
 		elif bricks.br.colliderect(circles.cr) and circles.x>((controls.mx+bricks.bwith/2)+controls.mx)/2 and circles.x<controls.mx+bricks.bwith/2 and not circles.y<my-circles.radius:
 			circles.direction = 2
 			circles.x_angle -= x_angle_change
 			circles.y_angle += y_angle_change
-			pass
+	
 		elif bricks.br.colliderect(circles.cr) and circles.x<((controls.mx+bricks.bwith/2)+bricks.bwith)/2 and not circles.y<my-circles.radius:
 			circles.direction = 3
 			circles.x_angle -= x_angle_change
 			circles.y_angle += y_angle_change
-			pass
+	
 		elif bricks.br.colliderect(circles.cr) and circles.x>((controls.mx+bricks.bwith/2)+bricks.bwith)/2 and not circles.y<my-circles.radius:
 			circles.direction = 3
 			circles.x_angle += x_angle_change
 			circles.y_angle -= y_angle_change
-			pass
-		pass
-	pass
+	def circle_collide_brs():
+		if circles.direction == 0 or circles.direction == 2:
+			circles.direction = 1
+		elif circles.direction == 1 or circles.direction == 3: 
+			circles.direction = 2
+		elif circles.direction == 2 or circles.direction == 0: 
+			circles.direction = 3
+		elif circles.direction == 1 or circles.direction == 3:
+			circles.direction = 0
 
-	pass
+
 class controls(object):
 	mx=12
 	"""docstring for controls"""
 	def mouse():
 		mouse_x , mouse_y = pygame.mouse.get_pos()
 		if pygame.mouse.get_pressed()[0] and bricks.br.collidepoint(pygame.mouse.get_pos()):
-			bricks.brick(mouse_x-bricks.bwith/2, my,bricks.bwith,50)
+			bricks.brick(mouse_x-bricks.bwith/2, my,bricks.bwith,20)
 			controls.mx = mouse_x-bricks.bwith/2
 			##my = mouse_y
-			pass
+	
 		else:
-			bricks.brick(controls.mx, my,bricks.bwith,50)
-			pass
-		pass
-	pass
+			bricks.brick(controls.mx, my,bricks.bwith,20)
+	def keyboard():
+		keys=pygame.key.get_pressed()
+		if keys[pygame.K_LEFT]:
+		    controls.mx -= 0.5
+		if keys[pygame.K_RIGHT]:
+		    controls.mx += 0.5
 
 
-run=True
-while run:
+while runn:
 	pygame.time.delay(1)
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT: run = False
-		pass
+		if event.type == pygame.QUIT: runn = False
 	win.fill((0,0,0))
 	controls.mouse()
+	controls.keyboard()
 	circles.circle_move()
-	keys = pygame.key.get_pressed()
-	pygame.display.update()	
+	if circles.y>resolution-circles.radius-1:
+		runn = False
+	bricks.brickss()
+	if len(bricks.brs) == len(bricks.brs_rm): 
+		runn = False
+	bricks.brs.clear()
+	pygame.display.update()
 pygame.quit()
